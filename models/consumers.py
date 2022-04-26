9a609883f5a67f35756552d4ed5c5a70b3f517da
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-04-26 20:08:52 -0300

    creted model city and consumers

---
+++ models/consumers.py
@@ -0,0 +1,8 @@
+class Consumers:
+    def __init__(self, name, cellphone,ip,city_id,problem_id ):
+        self.name = name
+        self.cellphone = cellphone
+        self.ip = ip
+        self.city_id = city_id
+        self.problem_id = problem_id
+    
