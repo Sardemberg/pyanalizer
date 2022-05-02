c5251ef4f8a1d9a265f73bf04797781c2938d51d
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-05-02 14:37:47 -0300

    creted controllers city and customers

---
+++ controllers/CityDAO.py
@@ -0,0 +1,28 @@
+
+import pathlib
+import sqlite3
+
+
+class CityDAO:
+    def __init__(self):
+         self.connection = sqlite3.connect(f"{pathlib.Path().resolve()}/db/banco.sql", isolation_level=None)
+         self.cursor = self.connection.cursor()
+
+    def create(self, City):
+        query = f"insert into cities (name) values ('{City.name}')"
+        self.cursor.execute(query)
+        print("Insert new city with status success")
+    
+    def delete(self, city_id ):
+        query = f"delete from cities where id = '{city_id}'"
+        self.cursor.execute(query)
+        print("Delete city with status success")
+
+    def show_all(self):
+        query = "select from * cities"
+        for row in self.cursor.execute(query).fetchall:
+            print(f'Name: {row}')
+      
+     
+    
+
---
+++ controllers/CustomersDAO.py
@@ -0,0 +1,32 @@
+
+import pathlib
+import sqlite3
+
+
+class CustomersDAO:
+    def __init__(self):
+         self.connection = sqlite3.connect(f"{pathlib.Path().resolve()}/db/banco.sql", isolation_level=None)
+         self.cursor = self.connection.cursor()
+
+    def create(self, Customers):
+        query = f"insert into customers (name) values ('{Customers.name}','{Customers.cellphone}','{Customers.ip}','{Customers.city_id}','{Customers.problem_id}')"
+        self.cursor.execute(query)
+        print("Insert new customer with status success")
+    
+    def delete(self, customers_id ):
+        query = f"delete from customers where id = '{customers_id}'"
+        self.cursor.execute(query)
+        print("Delete customer with status success")
+
+    def show_all(self):
+        query = "select from * customers"
+        for row in self.cursor.execute(query).fetchall:
+            print(f'Name: {row[0]}')
+            print(f'CellPhone: {row[1]}')
+            print(f'Ip: {row[2]}')
+            print(f'City_id: {row[3]}')
+            print(f'Problem_id: {row[4]}')
+      
+     
+    
+
