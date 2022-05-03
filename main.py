2d9a7b46da32821079428db362217fb2c51f43dd
Lucas Sardemberg <lucassardem@gmail.com>
2022-05-02 21:27:36 -0300

    Feat: Add essential elements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from models.consumer import Consumer
 from controllers.CustomersDAO import CustomersDAO
+from models.consumer import Consumer
 
-controller_customer = CustomersDAO()
-consumer = Consumer(name="Carlos", cellphone=88981994454, ip="192.168.1.1", city_id=1, problem_id=1)
+customer = Consumer(name="Carlos", cellphone=88981994454, ip="192.168.1.1", city_id=1, problem_id=1)
+customer_controller = CustomersDAO()
 
-controller_customer.show_all()
+customer_controller.show_all()
