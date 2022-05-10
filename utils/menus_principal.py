034e43942c005204b063234a049dc8b9ccfce315
Edson Nunes <edsonnunes05@icloud.com>
2022-05-10 12:23:39 -0300

    menus

---
+++ utils/menus_principal.py
@@ -0,0 +1,12 @@
+def menu():
+    print("Selecione a opção que deseja realizar")
+    print("0 - Sair da aplicação")
+    print("1 - Iniciar anánlise")
+    print("2 - Problemas por região")
+    print("3 - Problemas por prioridade")
+    print("4 - Clientes na espera de uma resposta a cerca da solução probelam ")
+    print("5 - Problemas mais recorrentes")
+    print("6 - Listas de problemas geral")
+    
+menu()
+
