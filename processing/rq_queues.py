a112b1bba5f9d39ea94022e78bfc15571a697391
Sardemberg <lucassardem@gmail.com>
2022-06-04 16:32:11 -0300

    Feat: Design views

--- processing/rq_queues.py
+++ processing/rq_queues.py
@@ -1,16 +1,13 @@
-from rq import Queue, Worker
+from rq import Queue
 from processing.processes import Processes
 from controllers.CustomersDAO import CustomersDAO
 from redis import Redis
-from time import sleep
 
 class RQQueues:
     def __init__(self):
         self.redis = Redis(host='localhost', port=6379)
         self.queue_ips = Queue('ips', connection=self.redis)
         self.queue_area = Queue('area', connection=self.redis)
-        self.ip_worker = Worker([self.queue_ips], connection=self.redis, name='ips_worker')
-        self.area_worker = Worker([self.queue_area], connection=self.redis, name='area_worker')
         self.process = Processes()
 
     def enqueue_ips(self):
