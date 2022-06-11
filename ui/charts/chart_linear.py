5a31f2f07f0098608efdf2b90104695a9762688d
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-06-11 20:00:10 -0300

    add new list

--- ui/charts/chart_linear.py
+++ ui/charts/chart_linear.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class ChartLinear:
-    def __init__(self, master=None):
+    def __init__(self, master=None, problemas=[]):
         # criando a figura
         figura = plt.Figure(figsize=(6, 4), dpi=55,)
         figura.patch.set_facecolor('#F0F0F0')
@@ -15,11 +15,11 @@ class ChartLinear:
         np.random.seed(19680801)
 
         # configurações do gráfico
-        x = np.linspace(0, 2, 100)  # Sample data.
-        ax.plot(x, x, label='Resolvidos')  # Plot some data on the axes.
-        ax.plot(x, x**2, label='Pendentes')  # Plot more data on the axes...
-        # ... and some more.
-        ax.set_xlabel('Periodo')  # Add an x-label to the axes.
-        ax.set_ylabel('Quantidade')  # Add a y-label to the axes.
-        ax.set_title("Gráfico de eficiência")  # Add a title to the axes.
+
+        values = np.linspace(1, 10, 10)
+        x = np.linspace(1, 7, 10)
+        ax.plot(x, values, label='Problemas')
+        ax.set_xlabel('Dia da semana')
+        ax.set_ylabel('Quantidade')
+        ax.set_title("Gráfico de eficiência")
         ax.legend()
