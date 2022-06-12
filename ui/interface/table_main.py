dddbdf5456f01b16f1e876897b06fa91a5ccfe0a
Edson Nunes <edsonnunes05@icloud.com>
2022-06-12 14:48:14 -0300

    create table main

---
+++ ui/interface/table_main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from tkinter import ttk
+
+from controllers.CityDAO import CityDAO
+
+class TableMain:
+    
+    def __init__(self, master=None, dados_problemas=[]):
+        
+        frame = Frame(master)
+
+        scroll = Scrollbar(frame)
+        scroll.pack(side=RIGHT, fill=Y)
+
+        principal = Listbox(frame, yscrollcommand=scroll.set)
+
+        
+        dados = ttk.Treeview(frame, columns=("Id",
+            "Cidades", "N˚ Problemas", "Resolvidos", "Pendentes"), show="headings")
+        dados.column("Id", minwidth=0, width=100)
+        dados.column("Cidades", minwidth=0, width=200)
+        dados.column("N˚ Problemas", minwidth=0, width=100)
+        dados.column("Resolvidos", minwidth=0, width=100)
+        dados.column("Pendentes", minwidth=0, width=100)
+        dados.heading("Id", text="ID ")
+        dados.heading("Cidades", text="CIDADES ")
+        dados.heading("N˚ Problemas", text="N˚ PROBLEMAS ")
+        dados.heading("Resolvidos", text="RESOLVIDOS ")
+        dados.heading("Pendentes", text="PENDENTES ")
+        dados.pack(side=LEFT)
+
+        for (id, ci, np, re, pe) in dados_problemas:
+            dados.insert("", "end", values=(id, ci, np, re, pe))
+
+        scroll.config(command=dados.yview)
+        frame.place(x=10, y=50)
